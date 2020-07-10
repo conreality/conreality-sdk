4742a478bde8c632ba49b60eb4fc1ce37be52511
Arto Bendiken <arto@conreality.com>
2020-07-11 00:11:01 +0300

    Refactored the Zig build configuration.

--- build.zig
+++ build.zig
@@ -1,7 +1,15 @@
 // This is free and unencumbered software released into the public domain.
 
-const Builder = @import("std").build.Builder;
-const fmt = @import("std").fmt;
+const builtin = @import("builtin");
+const std = @import("std");
+const fmt = std.fmt;
+
+const Builder = std.build.Builder;
+const LibExeObjStep = std.build.LibExeObjStep;
+
+const library_name = "conreality";
+const library_api = "src/api.zig";
+const build_directory = "build";
 
 pub fn build(b: *Builder) !void {
     var pathBuffer: [64]u8 = undefined;
@@ -10,21 +18,29 @@ pub fn build(b: *Builder) !void {
 
     try b.makePath("build");
 
-    const static_lib = b.addStaticLibrary("conreality", "src/api.zig");
-    static_lib.setBuildMode(mode);
-    static_lib.linkLibC();
-    static_lib.setOutputDir("build");
-    static_lib.install();
-
     const static_step = b.step("static", "Build static libraries for this host");
+    const static_lib = static(b, version, mode);
     static_step.dependOn(&static_lib.step);
 
-    const shared_lib = b.addSharedLibrary("conreality", "src/api.zig", version);
-    shared_lib.setBuildMode(mode);
-    shared_lib.linkLibC();
-    shared_lib.setOutputDir("build");
-    shared_lib.install();
-
     const shared_step = b.step("shared", "Build shared libraries for this host");
+    const shared_lib = shared(b, version, mode);
     shared_step.dependOn(&shared_lib.step);
+}
+
+fn static(b: *Builder, version: builtin.Version, mode: builtin.Mode) *LibExeObjStep {
+    const lib = b.addStaticLibrary(library_name, library_api);
+    lib.setBuildMode(mode);
+    lib.linkLibC();
+    lib.setOutputDir(build_directory);
+    lib.install();
+    return lib;
+}
+
+fn shared(b: *Builder, version: builtin.Version, mode: builtin.Mode) *LibExeObjStep {
+    const lib = b.addSharedLibrary(library_name, library_api, version);
+    lib.setBuildMode(mode);
+    lib.linkLibC();
+    lib.setOutputDir(build_directory);
+    lib.install();
+    return lib;
 }

4543b260b08275e5bf3febfac3536231573c71c9
Arto Bendiken <arto@conreality.com>
2020-07-11 16:36:42 +0300

    Implemented cross-compilation targets.

--- Makefile
+++ Makefile
@@ -24,7 +24,7 @@ SOURCES      := VERSION build.zig $(wildcard src/*.zig src/*/*.zig src/*/*/*.zig
 
 # The default target:
 
-default: all
+default: static shared pkgconfig
 
 .PHONY: default
 
@@ -51,8 +51,6 @@ $(builddir)/lib$(PACKAGE).dll: $(SOURCES)
 	$(ZIG) build shared
 	@touch $@
 
-all: static shared pkgconfig
-
 static:
 	$(ZIG) build static
 
@@ -63,7 +61,55 @@ pkgconfig: $(builddir)/$(PACKAGE).pc
 
 include etc/pkgconfig/rules.mk
 
-.PHONY: all static shared pkgconfig
+.PHONY: static shared pkgconfig
+
+# Rules for cross-compilation:
+
+all: android linux macos windows
+
+.PHONY: all
+
+android: x86_64-android arm64-android arm-android
+
+arm-android:
+	$(ZIG) build android/arm
+
+arm64-android:
+	$(ZIG) build android/arm64
+
+x86_64-android:
+	$(ZIG) build android/x86_64
+
+.PHONY: android arm-android arm64-android x86_64-android
+
+linux: x86_64-linux arm64-linux arm-linux
+
+arm-linux:
+	$(ZIG) build linux/arm
+
+arm64-linux:
+	$(ZIG) build linux/arm64
+
+x86_64-linux:
+	$(ZIG) build linux/x86_64
+
+.PHONY: linux arm-linux arm64-linux x86_64-linux
+
+macosx: macos
+macos: x86_64-macos
+
+x86_64-macosx: x86_64-macos
+x86_64-macos:
+	$(ZIG) build macos/x86_64
+
+.PHONY: macos x86_64-macos macosx x86_64-macosx
+
+windows: x86_64-windows
+
+x86_64-windows:
+	$(ZIG) build windows/x86_64
+
+.PHONY: windows x86_64-windows
 
 # Rules for verification:
 
@@ -81,15 +127,16 @@ install-headers:
 	@[ -f c/Makefile ] && $(MAKE) -w -C c install || true
 	@[ -f cpp/Makefile ] && $(MAKE) -w -C cpp install || true
 
-install-static: $(builddir)/lib$(PACKAGE).a installdirs
-	install -c -m 0644 $< $(DESTDIR)$(libdir)
+install-static: installdirs
+	[ -f $(builddir)/lib$(PACKAGE).a ] && install -c -m 0644 $(builddir)/lib$(PACKAGE).a $(DESTDIR)$(libdir) || true
+	[ -f $(builddir)/lib$(PACKAGE).lib ] && install -c -m 0644 $(builddir)/lib$(PACKAGE).lib $(DESTDIR)$(libdir) || true
 
 install-shared: installdirs
 	[ -f $(builddir)/lib$(PACKAGE).so.$(VERSION) ] && install -c -m 0644 $(builddir)/lib$(PACKAGE).so.$(VERSION) $(DESTDIR)$(libdir) || true
 	[ -f $(builddir)/lib$(PACKAGE).$(VERSION).dylib ] && install -c -m 0644 $(builddir)/lib$(PACKAGE).$(VERSION).dylib $(DESTDIR)$(libdir) || true
-	[ -f $(builddir)/lib$(PACKAGE).dll ] && install -c -m 0644 $(builddir)/lib$(PACKAGE).dll $(DESTDIR)$(libdir) || true
-	[ -f $(builddir)/lib$(PACKAGE).lib ] && install -c -m 0644 $(builddir)/lib$(PACKAGE).lib $(DESTDIR)$(libdir) || true
-	[ -f $(builddir)/lib$(PACKAGE).pdb ] && install -c -m 0644 $(builddir)/lib$(PACKAGE).pdb $(DESTDIR)$(libdir) || true
+	[ -f $(builddir)/$(PACKAGE).dll ] && install -c -m 0644 $(builddir)/$(PACKAGE).dll $(DESTDIR)$(libdir) || true
+	[ -f $(builddir)/$(PACKAGE).lib ] && install -c -m 0644 $(builddir)/$(PACKAGE).lib $(DESTDIR)$(libdir) || true
+	[ -f $(builddir)/$(PACKAGE).pdb ] && install -c -m 0644 $(builddir)/$(PACKAGE).pdb $(DESTDIR)$(libdir) || true
 
 install-pkgconfig: $(builddir)/$(PACKAGE).pc installdirs
 	install -c -m 0644 $< $(DESTDIR)$(libdir)/pkgconfig
--- build.zig
+++ build.zig
@@ -2,8 +2,8 @@
 
 const builtin = @import("builtin");
 const std = @import("std");
-const fmt = std.fmt;
 
+const CrossTarget = std.zig.CrossTarget;
 const Builder = std.build.Builder;
 const LibExeObjStep = std.build.LibExeObjStep;
 
@@ -12,35 +12,144 @@ const library_api = "src/api.zig";
 const build_directory = "build";
 
 pub fn build(b: *Builder) !void {
-    var pathBuffer: [64]u8 = undefined;
+    var buffer: [256]u8 = undefined;
     const version = b.version(20, 0, 0); // TODO: parse the VERSION file
+
+    b.setPreferredReleaseMode(.ReleaseSafe);
     const mode = b.standardReleaseOptions();
 
-    try b.makePath("build");
+    try b.makePath(build_directory);
+    b.verbose = true;
 
     const static_step = b.step("static", "Build static libraries for this host");
-    const static_lib = static(b, version, mode);
+    const static_lib = static(b, null, version, mode);
     static_step.dependOn(&static_lib.step);
 
     const shared_step = b.step("shared", "Build shared libraries for this host");
-    const shared_lib = shared(b, version, mode);
+    const shared_lib = shared(b, null, version, mode);
     shared_step.dependOn(&shared_lib.step);
+
+    for (targets) |target| {
+        const build_dir = try std.fmt.bufPrint(buffer[0..], "{}/{}", .{ build_directory, target.id });
+        try b.makePath(build_dir);
+
+        const target_info = try CrossTarget.parse(.{
+            .arch_os_abi = target.arch_os_abi,
+            .cpu_features = target.mcpu,
+        });
+
+        const target_static_lib = static(b, target_info, version, mode);
+        target_static_lib.setOutputDir(build_dir);
+        target_static_lib.setTarget(target_info);
+        target_static_lib.setLibCFile(target.libc);
+
+        const target_shared_lib = shared(b, target_info, version, mode);
+        target_shared_lib.setOutputDir(build_dir);
+        target_shared_lib.setTarget(target_info);
+        target_shared_lib.setLibCFile(target.libc);
+
+        if (target_info.os_tag != null) {
+            switch (target_info.os_tag.?) {
+                .windows => {
+                    // See: https://docs.microsoft.com/en-us/cpp/c-runtime-library/crt-library-features
+                    target_shared_lib.linkSystemLibrary("libcmt");
+                    target_shared_lib.linkSystemLibrary("libvcruntime");
+                    target_shared_lib.linkSystemLibrary("libucrt");
+                },
+                else => {},
+            }
+        }
+
+        const target_step = b.step(target.id, "Build static/shared libraries for a target");
+        target_step.dependOn(&target_static_lib.step);
+        target_step.dependOn(&target_shared_lib.step);
+    }
 }
 
-fn static(b: *Builder, version: builtin.Version, mode: builtin.Mode) *LibExeObjStep {
-    const lib = b.addStaticLibrary(library_name, library_api);
-    lib.setBuildMode(mode);
+fn static(b: *Builder, target_info: ?CrossTarget, version: builtin.Version, mode: builtin.Mode) *LibExeObjStep {
+    const lib_name = if (target_info != null and target_info.?.os_tag != null) switch (target_info.?.os_tag.?) {
+        .windows => "lib" ++ library_name,
+        else => library_name,
+    } else library_name;
+    const lib = b.addStaticLibrary(lib_name, library_api);
+    lib.single_threaded = true;
+    lib.force_pic = true;
+    lib.bundle_compiler_rt = true;
     lib.linkLibC();
+    lib.setBuildMode(mode);
     lib.setOutputDir(build_directory);
     lib.install();
     return lib;
 }
 
-fn shared(b: *Builder, version: builtin.Version, mode: builtin.Mode) *LibExeObjStep {
+fn shared(b: *Builder, target_info: ?CrossTarget, version: builtin.Version, mode: builtin.Mode) *LibExeObjStep {
     const lib = b.addSharedLibrary(library_name, library_api, version);
-    lib.setBuildMode(mode);
+    lib.single_threaded = true;
+    lib.force_pic = true;
     lib.linkLibC();
+    lib.setBuildMode(mode);
     lib.setOutputDir(build_directory);
     lib.install();
     return lib;
 }
+
+const Target = struct {
+    id: []const u8,
+    arch_os_abi: []const u8,
+    mcpu: ?[]const u8 = null,
+    cflags: ?[]const u8 = null, // TODO: append to args
+    libc: ?[]const u8 = null,
+};
+
+const targets = [_]Target{
+    // Android:
+    Target{
+        .id = "android/arm",
+        .arch_os_abi = "arm-linux-android",
+        .mcpu = "generic+v7a+thumb2+vfp3d16",
+        .cflags = "-mfloat-abi=softfp",
+        .libc = "etc/libc/android-4.1/arm.txt",
+    },
+    Target{
+        .id = "android/arm64",
+        .arch_os_abi = "aarch64-linux-android",
+        .mcpu = "generic+reserve_x18",
+        .libc = "etc/libc/android-5.0/arm64.txt",
+    },
+    Target{
+        .id = "android/x86_64",
+        .arch_os_abi = "x86_64-linux-android",
+        .mcpu = "x86_64+mmx+sse+sse2+sse3+ssse3+sse4_1+sse4_2+popcnt",
+        .libc = "etc/libc/android-5.0/x86_64.txt",
+    },
+    // Linux:
+    Target{
+        .id = "linux/arm",
+        .arch_os_abi = "arm-linux-musleabihf",
+        .mcpu = "generic+v7a+thumb2+vfp3d16",
+        .cflags = "-mfloat-abi=softfp",
+    },
+    Target{
+        .id = "linux/arm64",
+        .arch_os_abi = "aarch64-linux-musl",
+        .mcpu = "generic+reserve_x18",
+    },
+    Target{
+        .id = "linux/x86_64",
+        .arch_os_abi = "x86_64-linux-musl",
+        .mcpu = "x86_64+mmx+sse+sse2+sse3+ssse3+sse4_1+sse4_2+popcnt",
+    },
+    // macOS:
+    Target{
+        .id = "macos/x86_64",
+        .arch_os_abi = "x86_64-native", // FIXME: cross-compiling doesn't pass -lSystem to `lld`
+        .mcpu = "x86_64+mmx+sse+sse2+sse3+ssse3+sse4_1+sse4_2+popcnt",
+    },
+    // Windows:
+    Target{
+        .id = "windows/x86_64",
+        .arch_os_abi = "x86_64-windows-msvc",
+        .mcpu = "x86_64+mmx+sse+sse2+sse3+ssse3+sse4_1+sse4_2+popcnt",
+        .libc = "etc/libc/windows/x64-msvc.txt",
+    },
+};
